0b6f58b472947f8ca0aad0b26f563bafbc6bb8a3
Ariel <arielduek@gmail.com>
2019-12-23 18:46:58 +0200

    some changes on isConnect and copy methods in Graph_Algo

--- src/algorithms/Graph_Algo.java
+++ src/algorithms/Graph_Algo.java
@@ -14,21 +14,21 @@ import utils.Point3D;
  */
 public class Graph_Algo implements graph_algorithms{
 
-	private graph ga;
+	private graph graphAlgo;
 
 	public Graph_Algo(){
-		this.ga= null;
+		this.graphAlgo= null;
 	}
 
 	@Override
 	public void init(graph g) {
-		this.ga = g;
+		this.graphAlgo = g;
+
 	}
 
 	@Override
 	public void init(String file_name) {
-
-
+		
 	}
 
 	@Override
@@ -38,62 +38,40 @@ public class Graph_Algo implements graph_algorithms{
 
 	@Override
 	public boolean isConnected() {
-	    if(ga.getV().isEmpty()) return false;
-		Iterator iter = ga.getV().iterator();
-		while (iter.hasNext()){
-			node_data curr = (node_data) iter.next();
-			curr.setTag(0);
+		if (graphAlgo.getV().isEmpty()) return true;
+		setZeroTag(graphAlgo);
+		int keyOfFirst = graphAlgo.getV().iterator().next().getKey();
+		DFSUtil(keyOfFirst);
+		for (node_data curr : graphAlgo.getV()){
+			if (curr.getTag()==0) return false;
 		}
-
-		Iterator iter2 = ga.getV().iterator();
-		node_data curr2 =  (node_data) iter2.next();
-		DFSUtil(curr2.getKey());
-		while (iter2.hasNext()){
-			if (curr2.getTag()==0) return false;
-			curr2 =  (node_data) iter2.next();
+		getTranspose(graphAlgo);
+		setZeroTag(graphAlgo);
+		DFSUtil(keyOfFirst);
+		for (node_data curr : graphAlgo.getV()){
+			if (curr.getTag()==0) return false;
 		}
-		getTranspose(ga);
-
-		Iterator iter4 = ga.getV().iterator();
-		while (iter4.hasNext()){
-			node_data curr = (node_data) iter4.next();
-			curr.setTag(0);
-		}
-		Iterator iter3 = ga.getV().iterator();
-		node_data curr3 =  (node_data) iter3.next();
-		DFSUtil(curr3.getKey());
-		while (iter3.hasNext()){
-			if (curr3.getTag()==0) return false;
-			curr3 =  (node_data) iter3.next();
-		}
-
-        Iterator iter5 = ga.getV().iterator();
-        while (iter5.hasNext()){
-            node_data curr = (node_data) iter5.next();
-            curr.setTag(0);
-        }
-
-		getTranspose(ga);
 		return true;
 	}
 
-	private void DFSUtil(int key)
+	void setZeroTag(graph ga){
+		for (node_data curr : ga.getV()){
+			curr.setTag(0);
+		}
+	}
+
+	void DFSUtil(int key)
 	{
-		ga.getNode(key).setTag(1);
-		ga.connect(key,key,0);
-		Iterator iter = ga.getE(key).iterator();
-		while (iter.hasNext()){
-			edge_data curr = (edge_data) iter.next();
-			if(ga.getNode(curr.getDest()).getTag()==0){
-				DFSUtil(curr.getDest());
-			}
+		graphAlgo.getNode(key).setTag(1);
+		graphAlgo.connect(key,key,0);
+		for (edge_data curr : graphAlgo.getE(key)){
+			if(graphAlgo.getNode(curr.getDest()).getTag()==0) DFSUtil(curr.getDest());
 		}
 	}
 
 	void getTranspose(graph ga){
-		Iterator iterV = ga.getV().iterator();
-		while (iterV.hasNext()){
-			node_data currV = (node_data) iterV.next();
+		for (node_data currV : ga.getV()){
+			ga.connect(currV.getKey(), currV.getKey(),0);
 			Iterator iterE = ga.getE(currV.getKey()).iterator();
 			edge_data currE;
 			while (iterE.hasNext()){
@@ -128,8 +106,14 @@ public class Graph_Algo implements graph_algorithms{
 
 	@Override
 	public graph copy() {
-		// TODO Auto-generated method stub
-		return null;
+		graph toCopy = new DGraph();
+		for (node_data currV : this.graphAlgo.getV()){
+			toCopy.addNode(currV);
+			for (edge_data currE : this.graphAlgo.getE(currV.getKey())){
+				toCopy.connect(currE.getSrc(),currE.getDest(),currE.getWeight());
+			}
+		}
+		return toCopy;
 	}
 
 	public static void main(String[] args) {
@@ -145,10 +129,11 @@ public class Graph_Algo implements graph_algorithms{
 		d.addNode(c);
 		d.connect(a.getKey(),b.getKey(),4);
 		d.connect(b.getKey(),c.getKey(),50);
-//		d.connect(c.getKey(),a.getKey(),50);
+		d.connect(c.getKey(),a.getKey(),50);
 		Graph_Algo nt = new Graph_Algo();
 		nt.init(d);
-		System.out.println(nt.isConnected());
+		graph copied = nt.copy();
+				System.out.println(nt.isConnected());
 	}
 
 }
