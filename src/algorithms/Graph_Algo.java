f5a082380bc67f641c178daaa67f24aaec7d3c2a
tomlatin <tomlatin52@gmail.com>
2019-12-23 08:20:33 +0200

    we fix bug in isConnected

--- src/algorithms/Graph_Algo.java
+++ src/algorithms/Graph_Algo.java
@@ -27,7 +27,8 @@ public class Graph_Algo implements graph_algorithms{
 
 	@Override
 	public void init(String file_name) {
-		
+
+
 	}
 
 	@Override
@@ -37,11 +38,13 @@ public class Graph_Algo implements graph_algorithms{
 
 	@Override
 	public boolean isConnected() {
+	    if(ga.getV().isEmpty()) return false;
 		Iterator iter = ga.getV().iterator();
 		while (iter.hasNext()){
 			node_data curr = (node_data) iter.next();
 			curr.setTag(0);
 		}
+
 		Iterator iter2 = ga.getV().iterator();
 		node_data curr2 =  (node_data) iter2.next();
 		DFSUtil(curr2.getKey());
@@ -50,6 +53,7 @@ public class Graph_Algo implements graph_algorithms{
 			curr2 =  (node_data) iter2.next();
 		}
 		getTranspose(ga);
+
 		Iterator iter4 = ga.getV().iterator();
 		while (iter4.hasNext()){
 			node_data curr = (node_data) iter4.next();
@@ -62,13 +66,21 @@ public class Graph_Algo implements graph_algorithms{
 			if (curr3.getTag()==0) return false;
 			curr3 =  (node_data) iter3.next();
 		}
+
+        Iterator iter5 = ga.getV().iterator();
+        while (iter5.hasNext()){
+            node_data curr = (node_data) iter5.next();
+            curr.setTag(0);
+        }
+
 		getTranspose(ga);
 		return true;
 	}
 
-	void DFSUtil(int key)
+	private void DFSUtil(int key)
 	{
 		ga.getNode(key).setTag(1);
+		ga.connect(key,key,0);
 		Iterator iter = ga.getE(key).iterator();
 		while (iter.hasNext()){
 			edge_data curr = (edge_data) iter.next();
@@ -133,7 +145,7 @@ public class Graph_Algo implements graph_algorithms{
 		d.addNode(c);
 		d.connect(a.getKey(),b.getKey(),4);
 		d.connect(b.getKey(),c.getKey(),50);
-		d.connect(c.getKey(),a.getKey(),50);
+//		d.connect(c.getKey(),a.getKey(),50);
 		Graph_Algo nt = new Graph_Algo();
 		nt.init(d);
 		System.out.println(nt.isConnected());
