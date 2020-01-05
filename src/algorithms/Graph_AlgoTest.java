c8b6044a90df7ff520612c48bc75db672faf5516
Ariel <arielduek@gmail.com>
2020-01-05 21:38:31 +0200

    create Graph_AlgoTest

---
+++ src/algorithms/Graph_AlgoTest.java
@@ -0,0 +1,56 @@
+package algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Graph_AlgoTest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void testInit() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void isConnected() {
+    }
+
+    @Test
+    public void setZeroTag() {
+    }
+
+    @Test
+    public void DFSUtil() {
+    }
+
+    @Test
+    public void getTranspose() {
+    }
+
+    @Test
+    public void shortestPathDist() {
+    }
+
+    @Test
+    public void shortestPath() {
+    }
+
+    @Test
+    public void TSP() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
